90df598a26451cb4ca64ad719d03da3cb7f02df9
Arikaim <info@arikaim.com>
2022-08-08 19:21:30 +0300

    =

--- view/components/admin/type/type.js
+++ view/components/admin/type/type.js
@@ -1,6 +1,6 @@
 /**
  *  Arikaim
- *  @copyright  Copyright (c) Konstantin Atanasov <info@arikaim.com>
+ *  @copyright  Copyright (c) Intersoft Ltd <info@arikaim.com>
  *  @license    http://www.arikaim.com/license
  *  http://www.arikaim.com
  */
