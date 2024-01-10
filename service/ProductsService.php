2dcccabeb6085006116c8fc53838781e5f6930f0
Arikaim <info@arikaim.com>
2024-01-10 15:59:32 +0200

    =

--- service/ProductsService.php
+++ service/ProductsService.php
@@ -29,6 +29,18 @@ class ProductsService extends Service implements ServiceInterface
     }
 
     /**
+     * Find product
+     *
+     * @param mixed       $key
+     * @param integer|null $userId
+     * @return object|null
+     */
+    public function find($key, ?int $userId = null): ?object
+    {
+        return Model::Products('products')->findProduct($key,$userId);
+    }
+
+    /**
      * Find product by type
      *
      * @param string $type
