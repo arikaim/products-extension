fa29f4aee0a011cebab29d9faa9cc264db1d2e9e
Arikaim <info@arikaim.com>
2024-01-17 21:42:59 +0200

    =

---
+++ controllers/traits/Options.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Arikaim
+ *
+ * @link        http://www.arikaim.com
+ * @copyright   Copyright (c)  Konstantin Atanasov <info@arikaim.com>
+ * @license     http://www.arikaim.com/license
+ * 
+*/
+namespace Arikaim\Extensions\Products\Controllers\Traits;
+
+use Arikaim\Core\Db\Model;
+use Arikaim\Core\Paginator\Paginator;
+
+/**
+ * Products list trait
+*/
+trait Products 
+{
+    /**
+     * Update product options
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param Validator $data
+     * @return Psr\Http\Message\ResponseInterface
+    */
+    public function updateOptions($request, $response, $data) 
+    {         
+        $data
+            ->validate(true);
+
+        $prices = $data->get('price',null);
+
+        $product = Model::Products('products')->findById($data['uuid']); 
+        if ($product == null) {
+            $this->error('errors.id','Not valid product id');
+            return false;
+        }
+        
+        // check access
+        $this->requireUserOrControlPanel($product->user_id);
+        
+        $data->offsetUnset('uuid');
+        $options = Model::ProductOptions('products');
+
+        foreach ($data->toArray() as $key => $value) {
+            $options->saveOption($product->id,$key,$value);
+        }
+         
+        $this->setResponse(true,function() use($product) {                  
+            $this
+                ->message('options.update')
+                ->field('uuid',$product->uuid);                  
+        },'errors.options.update');                                    
+    }
+
+   
+}
